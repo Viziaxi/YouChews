03c8296df171b3d12209a03393ff95d93e0cfba8
Guanhong Chen <gc6624982@gmail.com>
2025-11-16 22:58:54 -0800

    changed to ES, Add restaurant_operations.js

--- api-server/config.js
+++ api-server/config.js
@@ -5,7 +5,7 @@ const config = {
   databaseUrl: process.env.DATABASE_URL,
   port: process.env.PORT || 3000,
   jwt:{
-      secret:process.env.JWT_SECRET,
+      secret:process.env.JWT_SECR
   },
 };
 export default config
