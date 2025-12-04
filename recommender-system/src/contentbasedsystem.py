55c754c0ce85da108c9302055906910ddd9b0df8
Guanhong Chen <gchen2@scu.edu>
2025-12-03 19:12:57 -0800

    changed to ES, Add restaurant_operations.js

--- recommender-system/src/contentbasedsystem.py
+++ recommender-system/src/contentbasedsystem.py
@@ -5,10 +5,12 @@ from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 def eval_features(data: pd.DataFrame, *args):
     for feature in args:
         if data[feature].dtype == str:
             data[feature] = data[feature].fillna("[]").apply(literal_eval)
+
 
 def prepare_strings(x):
     if isinstance(x, list):
@@ -18,9 +20,10 @@ def prepare_strings(x):
     else:
         return ""
 
+
 def create_vector_string(data) -> str:
     result = ""
-    for col in ["flavors", "menu", "name", "service_style"]:
+    for col in ["flavors", "menu", "name", "price", "service_style"]:
         if len(result) > 0:
             result += ' '
         if isinstance(data[col], str):
@@ -30,25 +33,26 @@ def create_vector_string(data) -> str:
 
     return result
 
+
 def find_next(content: pd.DataFrame, userdata: pd.DataFrame, num: int = 1) -> list[int]:
     userdata["item_id"] = userdata["item_id"].astype(int)
     userdata["like_level"] = userdata["like_level"].astype(float)
     userdata.sort_values("like_level", ascending=False, inplace=True, ignore_index=True)
-    
-    for col in ("flavors", "menu", "name", "service_style", "cuisine"):
+
+    for col in ("flavors", "menu", "name", "price", "service_style", "cuisine"):
         content[col] = content[col].apply(prepare_strings)
-    
+
     eval_features(content, "flavors", "menu")
 
-    #print(content["id"].dtype)
-    #print(userdata["item_id"].dtype)
-    #print(content.to_string())
-    #print(userdata.to_string())
-    
+    # print(content["id"].dtype)
+    # print(userdata["item_id"].dtype)
+    # print(content.to_string())
+    # print(userdata.to_string())
+
     selected: pd.DataFrame = content[content["id"].isin(userdata["item_id"])]
     contentvectors = content.apply(create_vector_string, axis=1)
     uservectors = selected.apply(create_vector_string, axis=1)
-    #print(selected.to_string())
+    # print(selected.to_string())
 
     contentcount = CountVectorizer(stop_words="english")
     contentmatrix = contentcount.fit_transform(contentvectors)
@@ -56,16 +60,16 @@ def find_next(content: pd.DataFrame, userdata: pd.DataFrame, num: int = 1) -> li
     usermatrix = usercount.fit_transform(uservectors)
     sim_matrix = cosine_similarity(usermatrix, contentmatrix)
 
-    #print(sim_matrix)
+    # print(sim_matrix)
 
     source_id = userdata["item_id"][0]
     source_content_row = content.index[content["id"] == source_id][0]
     source_userdata_row = userdata.index[userdata["item_id"] == source_id][0]
-    #print(f"Because you liked {original["name"][source_content_row]}:")
+    # print(f"Because you liked {original["name"][source_content_row]}:")
     indexed_scores = [pair for pair in enumerate(sim_matrix[source_userdata_row]) if pair[0] != source_content_row]
     indexed_scores = sorted(indexed_scores, key=lambda pair: pair[1], reverse=True)
     num = min(num, len(indexed_scores))
     best_rows = [pair[0] for pair in indexed_scores[:num]]
-    
-    #print([original["name"][row] for row in best_rows])
+
+    # print([original["name"][row] for row in best_rows])
     return [content["id"][row] for row in best_rows]
--- recommender-system/src/main.py
+++ recommender-system/src/main.py
@@ -5,12 +5,13 @@ import json
 import configparser
 import pathlib
 import psycopg2
-from io import StringIO
+import json
 
 import contentbasedsystem
 
 CONFIG_PATH = pathlib.Path(__file__).parent / "config.ini"
 TEST_PATH = pathlib.Path(__file__).parent / "testdata"
+
 
 def connect_database():
     try:
@@ -19,43 +20,43 @@ def connect_database():
             user="postgres",
             password="12345678",
             host="localhost",
-            port=5432
+            port=5432,
         )
     except:
         return False
+
 
 def use_testdata(count: int) -> list[int]:
     restaurants = pd.read_csv((TEST_PATH / "restaurants.csv").resolve(), quotechar='"', index_col=False)
     userdata = pd.read_csv((TEST_PATH / "userdata.csv").resolve(), quotechar='"', index_col=False)
     return contentbasedsystem.find_next(restaurants, userdata, count)
 
+
 def execute_from_args(connection) -> list[int]:
-    restaurant_ids_json = sys.argv[1]
-    restaurant_ids = json.loads(restaurant_ids_json)  # Parse JSON array to Python list
+    restaurant_ids = json.loads(sys.argv[1])
     user_id = int(sys.argv[2])
     item_count = int(sys.argv[3])
 
     content_rows: list[tuple]
     content_columns: list[str]
     userdata_rows: list[tuple]
-    userdata_columns: list[str]
 
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM restaurants WHERE id = ANY($1::int[])", (restaurant_ids,))
+        cursor.execute("SELECT * FROM restaurants WHERE id = ANY(%s)", (restaurant_ids,))
         content_columns = [desc[0] for desc in cursor.description]
         content_rows = cursor.fetchall()
     with connection.cursor() as cursor:
-        cursor.execute("SELECT user_preferences FROM users WHERE id = $1", user_id)
-        userdata_columns = [desc[0] for desc in cursor.description]
+        cursor.execute("SELECT user_preferences FROM users WHERE id = %s", (user_id,))
         userdata_rows = cursor.fetchall()
-    
+
     restaurant_table = pd.DataFrame(content_rows, columns=content_columns)
-    content = pd.concat(restaurant_table["restaurant_info"])
+    content = pd.DataFrame(restaurant_table["restaurant_info"].to_list())
     content["id"] = restaurant_table["id"]
 
-    userdata = pd.DataFrame(userdata_rows, columns=userdata_columns)
+    userdata = pd.DataFrame(userdata_rows[0][0])
 
     return contentbasedsystem.find_next(content, userdata, item_count)
+
 
 class NpEncoder(json.JSONEncoder):
     def default(self, o):
@@ -67,6 +68,7 @@ class NpEncoder(json.JSONEncoder):
             return o.tolist()
         return super(NpEncoder, self).default(o)
 
+
 def main():
     if not CONFIG_PATH.resolve().exists():
         config = configparser.ConfigParser()
@@ -77,7 +79,7 @@ def main():
 
         with open(CONFIG_PATH.resolve(), 'w') as configfile:
             config.write(configfile)
-    
+
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH.resolve())
     id_list = None
@@ -91,10 +93,11 @@ def main():
             connection.close()
         else:
             print("Unable to connect to database")
-    
+
     print(json.dumps(id_list, cls=NpEncoder))
     sys.stdout.flush()
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
